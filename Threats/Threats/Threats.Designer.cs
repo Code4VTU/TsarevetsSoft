548a3bd5803432a7983938c70e87063cde3c9328
kalinanb <kalinanb@gmail.com>
2016-03-26 12:41:36 +0200

    Add source code. Working map with markers.

---
+++ Threats/Threats/Threats.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Threats
+{
+    partial class Threats
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(-2, -3);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 17;
+            this.gmap.MinZoom = 3;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(932, 396);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 7D;
+            // 
+            // Threats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 393);
+            this.Controls.Add(this.gmap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Threats";
+            this.Text = "Threats";
+            this.Load += new System.EventHandler(this.Threats_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+
+
+    }
+}
+
---
+++ Threats/Threats/Threats.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+
+namespace Threats
+{
+    public partial class Threats : Form
+    {
+        public Threats()
+        {
+            InitializeComponent();
+        }
+
+        private void Threats_Load(object sender, EventArgs e)
+        {
+            /*Ping ping = new Ping();
+            //ping.Send("http://maps.google.com");
+            PingOptions ops = new PingOptions();
+            ops.DontFragment = true;
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buff = Encoding.ASCII.GetBytes (data);
+            int timeout = 10;
+            PingReply reply = ping.Send ("http://maps.google.com", timeout, buff, ops);
+            if (reply.Status == IPStatus.Success)
+            {*/
+                gmap.DragButton = MouseButtons.Left;
+                gmap.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+                GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+                gmap.EmptyMapBackground = System.Drawing.Color.DarkGray;
+                gmap.SetPositionByKeywords("Shipka, Bulgaria");
+            //}
+            gmap.ShowCenter = false;
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(42.696552, 23.32601), GMarkerGoogleType.blue_pushpin);
+            gmap.Overlays.Add(markersOverlay);
+            markersOverlay.Markers.Add(marker);
+            gmap.FillEmptyTiles = true;
+        }
+        
+        
+    }
+}
